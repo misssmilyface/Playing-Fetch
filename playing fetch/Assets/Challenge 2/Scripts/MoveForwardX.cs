97c9b5965016321fcfa10ef9e28d71279e6c40e2
misssmilyface <niafreya123@gmail.com>
2023-11-21 03:55:22 +0800

    Playing Fetch

---
+++ playing fetch/Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+    public float speed;
+    //private float time = 2;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ playing fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {   
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
